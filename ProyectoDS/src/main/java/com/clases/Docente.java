ba30eccba3ce71e75dc6f3be21b0c8b055c9582c
Huguito <110265653+BondiolaAlHorno@users.noreply.github.com>
2025-07-07 13:00:21 -0300

    correccion en el tipo de dato de los atributos de docente

--- ProyectoDS/src/main/java/com/clases/Docente.java
+++ ProyectoDS/src/main/java/com/clases/Docente.java
@@ -13,7 +13,7 @@ public class Docente extends  Persona{
     private List<AsistenciaDocente> asistencia;
     private List<MateriaImplementada> materia;
 
-    public Docente(String nombre,String apellido,String dni,Integer legajo,List<Asistencia> asistencia,MateriaImplementada materia){
+    public Docente(String nombre,String apellido,String dni,Integer legajo,List<AsistenciaDocente> asistencia,List<MateriaImplementada> materia){
         super(nombre, apellido, dni);
         this.legajo = legajo;
         this.asistencia = asistencia;
@@ -21,12 +21,12 @@ public class Docente extends  Persona{
     }
 
     //Método para crear un nuevo docente
-    public static Docente altaDocente(String nombre,String apellido,String dni,Integer legajo,List<Asistencia> asistencia,MateriaImplementada materia) {
+    public static Docente altaDocente(String nombre,String apellido,String dni,Integer legajo,List<AsistenciaDocente> asistencia,List<MateriaImplementada> materia) {
         return new Docente(nombre, apellido, dni, legajo, asistencia, materia);
     }
 
     //Método de instancia para modificar un docente existente
-    public void modificarDocente(String nombre,String apellido,String dni,Integer legajo,List<Asistencia> asistencia,MateriaImplementada materia) {
+    public void modificarDocente(String nombre,String apellido,String dni,Integer legajo,List<AsistenciaDocente> asistencia,List<MateriaImplementada> materia) {
         this.setNombre(nombre);
         this.setApellido(apellido);
         this.setDni(dni);
